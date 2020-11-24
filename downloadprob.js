87ae9f064307c8d33126f7d8dde2bc50b28e34a5
Akshay Agarwal <mathgeek145@gmail.com>
2020-11-24 20:32:29 +0800

    further changes

--- downloadprob.js
+++ downloadprob.js
@@ -13,15 +13,59 @@ const PORT = 10043;
 
 
 
-
+let data; 
+let starts = false; 
 app.post('/', (req, res) =>{
-    const data = req.body; 
-    console.log(`Problem Name: ${data.name}`); 
-    console.log(`Problem Group: ${data.group}`); 
-    console.log('Full body:'); 
-    console.log(JSON.stringify(data, null, 4)); 
-});
- 
+   data = req.body;  
+    starts = true; 
+    
+    all_tests = JSON.stringify(data.tests);  
+ console.log(all_tests); 
+ //   console.log(all_tests[11]); 
+    let start_input = 11; 
+
+    let cur_pos = start_input; 
+ //   console.log(cur_pos); 
+    while(all_tests[cur_pos] != ","){
+        cur_pos++; 
+       // console.log(data[cur_pos]); 
+    }
+    
+    cur_pos-=2; 
+
+    let input = ""; 
+
+    for(let i = start_input; i<=cur_pos; i++){
+        if(all_tests[i] != "\\" && all_tests[i] != "n"){
+            input+=all_tests[i]; 
+        }
+        else if(all_tests[i] == "n"){
+            input+=' '; 
+        }
+    }
+    console.log(input); 
+    // Output starting is 14 more than end of input 
+
+    let start_output = cur_pos + 13; 
+    cur_pos = start_output; 
+
+    while(all_tests[cur_pos] != "}"){
+        cur_pos++; 
+    }
+    cur_pos-=2; 
+    let output = ""; 
+    
+    for(let i = start_output; i<=cur_pos; i++){
+        if(all_tests[i] != "\\" && all_tests[i] != "n"){
+            output+=all_tests[i]; 
+        }
+        else if(all_tests[i] == "n"){
+            output+=' '; 
+        }
+    }
+    console.log(output); 
+
+}); 
 app.listen(PORT, err=>{
     if(err){
         console.error(err); 
